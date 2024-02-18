ccd312345256194ffd6d7cc6480eb8afafea8d05
Sina Sedaghat <sina@Sinas-MacBook-Pro.local>
2024-02-18 19:30:59 +0330

    create new component for show fav city data in favorite page

---
+++ src/components/FavoriteCard.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import type { FavData } from '@/types/favorites'
+interface Props {
+  favorites: FavData
+}
+
+// const props = defineProps<Props>()
+const props = defineProps<Props>()
+
+// const props = withDefaults(defineProps<Props>(), {
+//   msg: 'hello',
+//   labels: () => ['one', 'two']
+// })
+</script>
+
+<template>
+  <h3>favorite cart</h3>
+  {{ props.favorites }}
+</template>
--- src/views/main/FavoritesView.vue
+++ src/views/main/FavoritesView.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import FavoriteCard from '@/components/FavoriteCard.vue';
 import { useFavorites } from '@/composables/favorites';
 import { useFavoritesCitiesStore } from '@/stores/favorites_cities'
 
@@ -9,5 +10,10 @@ const favoritesCities = useFavoritesCitiesStore()
 <template>
   FavoritesView ===> {{ favCities }}
   <h2>Favorites</h2>
-  {{ favoritesCities.favData }}
+  <!-- {{ favoritesCities.favData }} -->
+  <div v-for="fav in favoritesCities.favData" :key="fav.id">
+    <FavoriteCard 
+      :favorites="fav"
+    />
+  </div>
 </template>
